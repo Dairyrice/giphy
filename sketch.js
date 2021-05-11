c05fa292ecb5ada5a3d1d815ea940bce3e329dcd
Dairyrice <derek.bui@sjsu.edu>
2021-05-11 13:19:32 -0700

    giph

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var api = "https://api.giphy.com/v1/gifs/search?";
+
+var apiKey = "&api_key=0UG01kS1ABG8wu8AcbEL7z1Hm6DvfBDj";
+
+var query = "&q=rainbow";
+
+
+function setup() {
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+}
+
+function gotData(giphy) {
+  for (var i = 0; i < giphy.data.length; i++){
+  createImg(giphy.data[0].images.original.url);
+}
+}
+
+function draw() {
+ 
+}
